1eff120ac2f0f273fb2fff35c4ee698ef3831e38
Shahaf Israel <121295664+shahaf5641@users.noreply.github.com>
2026-05-03 09:56:03 +0300

    Add: is-palindrome - submission-2

---
+++ Data Structures & Algorithms/is-palindrome/submission-2.java
@@ -0,0 +1,24 @@
+class Solution {
+    public boolean isPalindrome(String s) {
+        /*1. Clear the string
+        2. 1 pointer from start and 2 pointer from end, while l<r*/
+
+
+        String filteredString = "";
+        for (char c : s.toCharArray()) {
+            if (Character.isLetter(c) || Character.isDigit(c))
+                filteredString += Character.toLowerCase(c);
+        }
+
+
+        int l = 0;
+        int r = filteredString.length() -1;
+
+        while (l<r) {
+            if (filteredString.charAt(l) != filteredString.charAt(r)) return false;
+            l+=1;
+            r-=1;
+        }
+        return true;
+    }
+}
